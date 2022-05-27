3d4a2e15d7243605f88d931f1d496da6fc471d6a
HippoGiant <100331681+HippoGiant@users.noreply.github.com>
2022-05-27 16:51:15 +0800

    連線到MySQL 將資料撈到後端

--- routes/Product/product.js
+++ routes/Product/product.js
@@ -2,9 +2,25 @@ var express = require('express');
 var router = express.Router();
 const db = require('../../modules/mysql_config');
 
-router.get('/', function (req, res, next) {
-    res.send('PRODUCT');
-});
+// router.get('/', function (req, res, next) {
+//     res.send('PRODUCT');
+// });
+
+router.route('/')
+.get(async(req,res,next)=>{
+    const sql = "SELECT * FROM product_main"
+    const [datas] = await db.query(sql)
+    res.json(datas)
+})
+
+// router.get('/',function(req,res,next){
+//     db.query(
+//         'SELECT * FROM product_main order by product_name',function(err,results,fields){
+//             res.json(results)
+//             console.log(results)
+//         }
+//     )
+// })
 
 
 
